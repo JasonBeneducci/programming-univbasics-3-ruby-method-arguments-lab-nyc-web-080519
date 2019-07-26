3398e479c294c395368f92ede294fc11b0d3a663
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-07-26 02:11:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,10 +8,6 @@ end
 
 def introduction_with_language_optional(name, language)
   
-  if language == nil
-    language == "Ruby"
-  end
-  
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
   
 end
